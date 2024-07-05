6ee5efa2125abaa5136baadfcdde11b6f4b3953d
rapha <dev@uupi.cloud>
2024-07-05 21:34:36 +0200

    chore: remove unused code

--- backend/types/jobs.go
+++ backend/types/jobs.go
@@ -2,7 +2,6 @@ package types
 
 import (
 	"fmt"
-	"time"
 )
 
 type BackupIdentifier struct {
@@ -23,28 +22,10 @@ type BackupJob struct {
 	IsQuiet      bool
 }
 
-type FinishBackupJob struct {
-	Id        BackupIdentifier
-	StartTime time.Time
-	EndTime   time.Time
-	Cmd       string
-	Err       error
-}
-
 type PruneJob struct {
 	Id           BackupIdentifier
 	RepoUrl      string
 	RepoPassword string
 	Prefix       string
 	BinaryPath   string
-}
-
-type FinishPruneJob struct {
-	Id         BackupIdentifier
-	StartTime  time.Time
-	EndTime    time.Time
-	PruneCmd   string
-	PruneErr   error
-	CompactCmd string
-	CompactErr error
 }
