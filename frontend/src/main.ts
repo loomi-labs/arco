d131bd9e792829823070b6a41436e30b827ef60a
rapha <dev@uupi.cloud>
2024-08-05 15:53:17 +0200

    feat: add i18n support

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,13 +4,24 @@ import './style.css';
 import router from './router'
 import Toast, { PluginOptions } from "vue-toastification";
 import "vue-toastification/dist/index.css";
+import { createI18n } from "vue-i18n";
+import en from './i18n/en.json';
 
 const app = createApp(App);
-
-app.use(router).mount('#app');
 
 const options: PluginOptions = {
   // Set options for the toast here
 };
 
+const i18n = createI18n({
+  locale: "en",
+  fallbackLocale: "en",
+  messages: {
+    en
+  }
+})
+
 app.use(Toast, options);
+app.use(i18n)
+app.use(router).mount('#app');
+
