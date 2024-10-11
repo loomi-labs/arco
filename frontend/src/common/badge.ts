e3269a3fd9bd57b1b93dee6a7e5283984b2dabe9
rapha <dev@uupi.cloud>
2024-10-11 15:50:28 +0200

    fix: badge size

--- frontend/src/common/badge.ts
+++ frontend/src/common/badge.ts
@@ -16,24 +16,24 @@ export function toDurationBadge(date: Date | undefined): string {
 
   const dHours = diffHours(now, date);
   if (dHours < 1) {
-    return "badge badge-outline text-warning";
+    return "badge badge-outline text-warning h-full";
   }
   const dDays = diffDays(now, date);
   if (dDays < 1) {
-    return "badge badge-outline text-success";
+    return "badge badge-outline text-success h-full";
   }
   if (dDays < 2) {
-    return "badge badge-outline text-blue-600 dark:text-blue-500";
+    return "badge badge-outline text-blue-600 dark:text-blue-500 h-full";
   }
   if (dDays < 7) {
-    return "badge badge-outline text-blue-900 dark:text-blue-400";
+    return "badge badge-outline text-blue-900 dark:text-blue-400 h-full";
   }
   if (dDays < 30) {
-    return "badge badge-outline text-gray-600 dark:text-blue-200";
+    return "badge badge-outline text-gray-600 dark:text-blue-200 h-full";
   }
   if (dDays < 365) {
-    return "badge badge-outline text-gray-500 dark:text-gray-400";
+    return "badge badge-outline text-gray-500 dark:text-gray-400 h-full";
   }
 
-  return "badge badge-outline text-gray-400 dark:text-gray-500";
+  return "badge badge-outline text-gray-400 dark:text-gray-500 h-full";
 }
