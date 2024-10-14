24bebc259d18734d3c2e4fd0d6a7b61ecd319d55
rapha <dev@uupi.cloud>
2024-10-14 16:52:58 +0200

    feat: add tooltip

---
+++ frontend/src/components/common/TooltipIcon.vue
@@ -0,0 +1,37 @@
+<script setup lang='ts'>
+
+import { QuestionMarkCircleIcon } from "@heroicons/vue/24/outline";
+
+/************
+ * Types
+ ************/
+
+interface Props {
+  text: string;
+}
+
+/************
+ * Variables
+ ************/
+
+defineProps<Props>();
+
+/************
+ * Functions
+ ************/
+
+/************
+ * Lifecycle
+ ************/
+
+</script>
+
+<template>
+  <span class='tooltip' :data-tip='text'>
+    <QuestionMarkCircleIcon class='size-6 text-gray-500' />
+  </span>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/pages/AddBackupProfilePage.vue
+++ frontend/src/pages/AddBackupProfilePage.vue
@@ -23,6 +23,7 @@ import FormField from "../components/common/FormField.vue";
 import { useForm } from "vee-validate";
 import * as yup from "yup";
 import SelectIconModal from "../components/SelectIconModal.vue";
+import TooltipIcon from "../components/common/TooltipIcon.vue";
 
 /************
  * Types
@@ -241,7 +242,9 @@ getExistingRepositories();
     <!-- 1. Step - Data Selection -->
     <template v-if='currentStep === Step.SelectData'>
       <!-- Data to backup Card -->
-      <h2 class='text-3xl py-4'>Data to backup</h2>
+      <h2 class='flex items-center gap-1 text-3xl py-4'>Data to backup
+        <TooltipIcon text='Select any folder or file that you want to include in your backup.' />
+      </h2>
       <DataSelection
         :paths='backupProfile.backupPaths'
         :suggestions='directorySuggestions'
@@ -253,7 +256,10 @@ getExistingRepositories();
         @update:is-valid='(isValid) => isBackupPathsValid = isValid' />
 
       <!-- Data to ignore Card -->
-      <h2 class='text-3xl pt-8 pb-4'>Data to ignore</h2>
+      <h2 class='flex items-center gap-1 text-3xl py-4'>Data to ignore
+<!--        https://borgbackup.readthedocs.io/en/stable/usage/help.html#borg-patterns-->
+        <TooltipIcon text="Select files, folders or patterns that you don't want to include in your backups. This supports wildcards. Example: *.cache -> exclude alls .cache folders" />
+      </h2>
       <DataSelection
         :paths='backupProfile.excludePaths'
         :is-backup-selection='false'
