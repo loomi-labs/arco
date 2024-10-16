1c05193d6f865c91057d319ddbe327d782da5e70
rapha <dev@uupi.cloud>
2024-10-16 14:11:59 +0200

    fix: make ExitError a pointer

--- backend/borg/error.go
+++ backend/borg/error.go
@@ -16,7 +16,7 @@ func (CancelErr) Error() string {
 }
 
 type WrappedExitErr struct {
-	ExitError exec.ExitError
+	ExitError *exec.ExitError
 }
 
 func (e WrappedExitErr) Error() string {
@@ -338,143 +338,143 @@ func exitCodesToError(err error) error {
 	// Return the error based on the exit code
 	switch exitError.ExitCode() {
 	case 2:
-		return Error{WrappedExitErr{ExitError: *exitError}}
+		return Error{WrappedExitErr{ExitError: exitError}}
 	case 3:
-		return CancelledByUser{WrappedExitErr{ExitError: *exitError}}
+		return CancelledByUser{WrappedExitErr{ExitError: exitError}}
 	case 4:
-		return CommandError{WrappedExitErr{ExitError: *exitError}}
+		return CommandError{WrappedExitErr{ExitError: exitError}}
 	case 5:
-		return PlaceholderError{WrappedExitErr{ExitError: *exitError}}
+		return PlaceholderError{WrappedExitErr{ExitError: exitError}}
 	case 6:
-		return InvalidPlaceholder{WrappedExitErr{ExitError: *exitError}}
+		return InvalidPlaceholder{WrappedExitErr{ExitError: exitError}}
 	case 10:
-		return RepositoryAlreadyExists{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryAlreadyExists{WrappedExitErr{ExitError: exitError}}
 	case 11:
-		return RepositoryAtticRepository{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryAtticRepository{WrappedExitErr{ExitError: exitError}}
 	case 12:
-		return RepositoryCheckNeeded{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryCheckNeeded{WrappedExitErr{ExitError: exitError}}
 	case 13:
-		return RepositoryDoesNotExist{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryDoesNotExist{WrappedExitErr{ExitError: exitError}}
 	case 14:
-		return RepositoryInsufficientFreeSpaceError{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryInsufficientFreeSpaceError{WrappedExitErr{ExitError: exitError}}
 	case 15:
-		return RepositoryInvalidRepository{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryInvalidRepository{WrappedExitErr{ExitError: exitError}}
 	case 16:
-		return RepositoryInvalidRepositoryConfig{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryInvalidRepositoryConfig{WrappedExitErr{ExitError: exitError}}
 	case 17:
-		return RepositoryObjectNotFound{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryObjectNotFound{WrappedExitErr{ExitError: exitError}}
 	case 18:
-		return RepositoryParentPathDoesNotExist{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryParentPathDoesNotExist{WrappedExitErr{ExitError: exitError}}
 	case 19:
-		return RepositoryPathAlreadyExists{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryPathAlreadyExists{WrappedExitErr{ExitError: exitError}}
 	case 20:
-		return RepositoryStorageQuotaExceeded{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryStorageQuotaExceeded{WrappedExitErr{ExitError: exitError}}
 	case 21:
-		return RepositoryPathPermissionDenied{WrappedExitErr{ExitError: *exitError}}
+		return RepositoryPathPermissionDenied{WrappedExitErr{ExitError: exitError}}
 	case 25:
-		return MandatoryFeatureUnsupported{WrappedExitErr{ExitError: *exitError}}
+		return MandatoryFeatureUnsupported{WrappedExitErr{ExitError: exitError}}
 	case 26:
-		return NoManifestError{WrappedExitErr{ExitError: *exitError}}
+		return NoManifestError{WrappedExitErr{ExitError: exitError}}
 	case 27:
-		return UnsupportedManifestError{WrappedExitErr{ExitError: *exitError}}
+		return UnsupportedManifestError{WrappedExitErr{ExitError: exitError}}
 	case 30:
-		return ArchiveAlreadyExists{WrappedExitErr{ExitError: *exitError}}
+		return ArchiveAlreadyExists{WrappedExitErr{ExitError: exitError}}
 	case 31:
-		return ArchiveDoesNotExist{WrappedExitErr{ExitError: *exitError}}
+		return ArchiveDoesNotExist{WrappedExitErr{ExitError: exitError}}
 	case 32:
-		return ArchiveIncompatibleFilesystemEncodingError{WrappedExitErr{ExitError: *exitError}}
+		return ArchiveIncompatibleFilesystemEncodingError{WrappedExitErr{ExitError: exitError}}
 	case 40:
-		return KeyfileInvalidError{WrappedExitErr{ExitError: *exitError}}
+		return KeyfileInvalidError{WrappedExitErr{ExitError: exitError}}
 	case 41:
-		return KeyfileMismatchError{WrappedExitErr{ExitError: *exitError}}
+		return KeyfileMismatchError{WrappedExitErr{ExitError: exitError}}
 	case 42:
-		return KeyfileNotFoundError{WrappedExitErr{ExitError: *exitError}}
+		return KeyfileNotFoundError{WrappedExitErr{ExitError: exitError}}
 	case 43:
-		return NotABorgKeyFile{WrappedExitErr{ExitError: *exitError}}
+		return NotABorgKeyFile{WrappedExitErr{ExitError: exitError}}
 	case 44:
-		return RepoKeyNotFoundError{WrappedExitErr{ExitError: *exitError}}
+		return RepoKeyNotFoundError{WrappedExitErr{ExitError: exitError}}
 	case 45:
-		return RepoIdMismatch{WrappedExitErr{ExitError: *exitError}}
+		return RepoIdMismatch{WrappedExitErr{ExitError: exitError}}
 	case 46:
-		return UnencryptedRepo{WrappedExitErr{ExitError: *exitError}}
+		return UnencryptedRepo{WrappedExitErr{ExitError: exitError}}
 	case 47:
-		return UnknownKeyType{WrappedExitErr{ExitError: *exitError}}
+		return UnknownKeyType{WrappedExitErr{ExitError: exitError}}
 	case 48:
-		return UnsupportedPayloadError{WrappedExitErr{ExitError: *exitError}}
+		return UnsupportedPayloadError{WrappedExitErr{ExitError: exitError}}
 	case 50:
-		return NoPassphraseFailure{WrappedExitErr{ExitError: *exitError}}
+		return NoPassphraseFailure{WrappedExitErr{ExitError: exitError}}
 	case 51:
-		return PasscommandFailure{WrappedExitErr{ExitError: *exitError}}
+		return PasscommandFailure{WrappedExitErr{ExitError: exitError}}
 	case 52:
-		return PassphraseWrong{WrappedExitErr{ExitError: *exitError}}
+		return PassphraseWrong{WrappedExitErr{ExitError: exitError}}
 	case 53:
-		return PasswordRetriesExceeded{WrappedExitErr{ExitError: *exitError}}
+		return PasswordRetriesExceeded{WrappedExitErr{ExitError: exitError}}
 	case 60:
-		return CacheCacheInitAbortedError{WrappedExitErr{ExitError: *exitError}}
+		return CacheCacheInitAbortedError{WrappedExitErr{ExitError: exitError}}
 	case 61:
-		return CacheEncryptionMethodMismatch{WrappedExitErr{ExitError: *exitError}}
+		return CacheEncryptionMethodMismatch{WrappedExitErr{ExitError: exitError}}
 	case 62:
-		return CacheRepositoryAccessAborted{WrappedExitErr{ExitError: *exitError}}
+		return CacheRepositoryAccessAborted{WrappedExitErr{ExitError: exitError}}
 	case 63:
-		return CacheRepositoryIDNotUnique{WrappedExitErr{ExitError: *exitError}}
+		return CacheRepositoryIDNotUnique{WrappedExitErr{ExitError: exitError}}
 	case 64:
-		return CacheRepositoryReplay{WrappedExitErr{ExitError: *exitError}}
+		return CacheRepositoryReplay{WrappedExitErr{ExitError: exitError}}
 	case 70:
-		return LockError{WrappedExitErr{ExitError: *exitError}}
+		return LockError{WrappedExitErr{ExitError: exitError}}
 	case 71:
-		return LockErrorT{WrappedExitErr{ExitError: *exitError}}
+		return LockErrorT{WrappedExitErr{ExitError: exitError}}
 	case 72:
-		return LockFailed{WrappedExitErr{ExitError: *exitError}}
+		return LockFailed{WrappedExitErr{ExitError: exitError}}
 	case 73:
-		return LockTimeout{WrappedExitErr{ExitError: *exitError}}
+		return LockTimeout{WrappedExitErr{ExitError: exitError}}
 	case 74:
-		return NotLocked{WrappedExitErr{ExitError: *exitError}}
+		return NotLocked{WrappedExitErr{ExitError: exitError}}
 	case 75:
-		return NotMyLock{WrappedExitErr{ExitError: *exitError}}
+		return NotMyLock{WrappedExitErr{ExitError: exitError}}
 	case 80:
-		return ConnectionClosed{WrappedExitErr{ExitError: *exitError}}
+		return ConnectionClosed{WrappedExitErr{ExitError: exitError}}
 	case 81:
-		return ConnectionClosedWithHint{WrappedExitErr{ExitError: *exitError}}
+		return ConnectionClosedWithHint{WrappedExitErr{ExitError: exitError}}
 	case 82:
-		return InvalidRPCMethod{WrappedExitErr{ExitError: *exitError}}
+		return InvalidRPCMethod{WrappedExitErr{ExitError: exitError}}
 	case 83:
-		return PathNotAllowed{WrappedExitErr{ExitError: *exitError}}
+		return PathNotAllowed{WrappedExitErr{ExitError: exitError}}
 	case 84:
-		return RemoteRepositoryRPCServerOutdated{WrappedExitErr{ExitError: *exitError}}
+		return RemoteRepositoryRPCServerOutdated{WrappedExitErr{ExitError: exitError}}
 	case 85:
-		return UnexpectedRPCDataFormatFromClient{WrappedExitErr{ExitError: *exitError}}
+		return UnexpectedRPCDataFormatFromClient{WrappedExitErr{ExitError: exitError}}
 	case 86:
-		return UnexpectedRPCDataFormatFromServer{WrappedExitErr{ExitError: *exitError}}
+		return UnexpectedRPCDataFormatFromServer{WrappedExitErr{ExitError: exitError}}
 	case 87:
-		return ConnectionBrokenWithHint{WrappedExitErr{ExitError: *exitError}}
+		return ConnectionBrokenWithHint{WrappedExitErr{ExitError: exitError}}
 	case 90:
-		return IntegrityError{WrappedExitErr{ExitError: *exitError}}
+		return IntegrityError{WrappedExitErr{ExitError: exitError}}
 	case 91:
-		return FileIntegrityError{WrappedExitErr{ExitError: *exitError}}
+		return FileIntegrityError{WrappedExitErr{ExitError: exitError}}
 	case 92:
-		return DecompressionError{WrappedExitErr{ExitError: *exitError}}
+		return DecompressionError{WrappedExitErr{ExitError: exitError}}
 	case 95:
-		return ArchiveTAMInvalid{WrappedExitErr{ExitError: *exitError}}
+		return ArchiveTAMInvalid{WrappedExitErr{ExitError: exitError}}
 	case 96:
-		return ArchiveTAMRequiredError{WrappedExitErr{ExitError: *exitError}}
+		return ArchiveTAMRequiredError{WrappedExitErr{ExitError: exitError}}
 	case 97:
-		return TAMInvalid{WrappedExitErr{ExitError: *exitError}}
+		return TAMInvalid{WrappedExitErr{ExitError: exitError}}
 	case 98:
-		return TAMRequiredError{WrappedExitErr{ExitError: *exitError}}
+		return TAMRequiredError{WrappedExitErr{ExitError: exitError}}
 	case 99:
-		return TAMUnsupportedSuiteError{WrappedExitErr{ExitError: *exitError}}
+		return TAMUnsupportedSuiteError{WrappedExitErr{ExitError: exitError}}
 	case 102:
-		return BackupError{WrappedExitErr{ExitError: *exitError}}
+		return BackupError{WrappedExitErr{ExitError: exitError}}
 	case 103:
-		return BackupRaceConditionError{WrappedExitErr{ExitError: *exitError}}
+		return BackupRaceConditionError{WrappedExitErr{ExitError: exitError}}
 	case 104:
-		return BackupOSError{WrappedExitErr{ExitError: *exitError}}
+		return BackupOSError{WrappedExitErr{ExitError: exitError}}
 	case 105:
-		return BackupPermissionError{WrappedExitErr{ExitError: *exitError}}
+		return BackupPermissionError{WrappedExitErr{ExitError: exitError}}
 	case 106:
-		return BackupIOError{WrappedExitErr{ExitError: *exitError}}
+		return BackupIOError{WrappedExitErr{ExitError: exitError}}
 	case 107:
-		return BackupFileNotFoundError{WrappedExitErr{ExitError: *exitError}}
+		return BackupFileNotFoundError{WrappedExitErr{ExitError: exitError}}
 	default:
 		return err
 	}
