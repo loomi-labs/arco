06fc3b402f5458ff30183244ce04071c2ab57944
rapha <dev@uupi.cloud>
2024-12-02 13:42:49 +0100

    fix: parsing error when having a warning in `info`

--- backend/borg/info.go
+++ backend/borg/info.go
@@ -1,6 +1,7 @@
 package borg
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -10,6 +11,25 @@ import (
 
 func (cr *commandRunner) Info(cmd *exec.Cmd) ([]byte, error) {
 	return cmd.CombinedOutput()
+}
+
+// sanitizeOutput removes all lines before the first line that starts with '{'
+func sanitizeOutput(out []byte) []byte {
+	out = bytes.TrimSpace(out)
+
+	// Nothing to sanitize
+	if bytes.HasPrefix(out, []byte("{")) {
+		return out
+	}
+
+	// Split the output into lines and find the first line that starts with '{'
+	lines := bytes.Split(out, []byte("\n"))
+	for i, line := range lines {
+		if bytes.HasPrefix(bytes.TrimSpace(line), []byte("{")) {
+			return bytes.Join(lines[i:], []byte("\n"))
+		}
+	}
+	return out
 }
 
 func (b *borg) Info(ctx context.Context, repository, password string) (*types.InfoResponse, error) {
@@ -24,7 +44,7 @@ func (b *borg) Info(ctx context.Context, repository, password string) (*types.In
 	}
 
 	var info types.InfoResponse
-	err = json.Unmarshal(out, &info)
+	err = json.Unmarshal(sanitizeOutput(out), &info)
 	if err != nil {
 		return nil, b.log.LogCmdError(ctx, cmd.String(), startTime, fmt.Errorf("failed to parse borg info output: %w", err))
 	}
--- backend/borg/info_test.go
+++ backend/borg/info_test.go
@@ -126,8 +126,8 @@ var nonEmptyRepo = testBorgInfo{
 		SecurityDir: "/home/test/.config/borg/security/90cec45b910252da33ab8a2c90019ecfc8ad12ec59fd18d6e41f315d59775a9a",
 	},
 }
-var repoWithWarning = testBorgInfo{
-	name: "Call info - empty repo",
+var withWarning = testBorgInfo{
+	name: "Call info - with warning",
 	cmdResult: []byte(`
 Using a pure-python msgpack! This will result in lower performance.
 {
@@ -178,6 +178,121 @@ Using a pure-python msgpack! This will result in lower performance.
 	wantErr: false,
 }
 
+var withMultilineWarning = testBorgInfo{
+	name: "Call info - multiline warning",
+	cmdResult: []byte(`
+Using a pure-python msgpack! This will result in lower performance.
+Using a pure-python msgpack! This will result in lower performance.
+Using a pure-python msgpack! This will result in lower performance.
+{
+    "cache": {
+        "path": "/home/test/.cache/borg/01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+        "stats": {
+            "total_chunks": 0,
+            "total_csize": 0,
+            "total_size": 0,
+            "total_unique_chunks": 0,
+            "unique_csize": 0,
+            "unique_size": 0
+        }
+    },
+    "encryption": {
+        "mode": "repokey-blake2"
+    },
+    "repository": {
+        "id": "01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+        "last_modified": "2024-12-02T10:28:45.000000",
+        "location": "/home/test/arcotest/dest/encrypted_with_123"
+    },
+    "security_dir": "/home/test/.config/borg/security/01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf"
+}`),
+	result: &types.InfoResponse{
+		Archives: nil,
+		Cache: types.Cache{
+			Path: "/home/test/.cache/borg/01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+			Stats: types.Stats{
+				TotalChunks:       0,
+				TotalCSize:        0,
+				TotalSize:         0,
+				TotalUniqueChunks: 0,
+				UniqueCSize:       0,
+				UniqueSize:        0,
+			},
+		},
+		Encryption: types.Encryption{
+			Mode: "repokey-blake2",
+		},
+		Repository: types.Repository{
+			ID:           "01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+			LastModified: "2024-12-02T10:28:45.000000",
+			Location:     "/home/test/arcotest/dest/encrypted_with_123",
+		},
+		SecurityDir: "/home/test/.config/borg/security/01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+	},
+	wantErr: false,
+}
+
+var withMultilineWarningAndSpaces = testBorgInfo{
+	name: "Call info - multiline warning with spaces",
+	cmdResult: []byte(`
+Using a pure-python msgpack! This will result in lower performance.
+Using a pure-python msgpack! This will result in lower performance.
+Using a pure-python msgpack! This will result in lower performance.
+  {
+    "cache": {
+        "path": "/home/test/.cache/borg/01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+        "stats": {
+            "total_chunks": 0,
+            "total_csize": 0,
+            "total_size": 0,
+            "total_unique_chunks": 0,
+            "unique_csize": 0,
+            "unique_size": 0
+        }
+    },
+    "encryption": {
+        "mode": "repokey-blake2"
+    },
+    "repository": {
+        "id": "01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+        "last_modified": "2024-12-02T10:28:45.000000",
+        "location": "/home/test/arcotest/dest/encrypted_with_123"
+    },
+    "security_dir": "/home/test/.config/borg/security/01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf"
+}`),
+	result: &types.InfoResponse{
+		Archives: nil,
+		Cache: types.Cache{
+			Path: "/home/test/.cache/borg/01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+			Stats: types.Stats{
+				TotalChunks:       0,
+				TotalCSize:        0,
+				TotalSize:         0,
+				TotalUniqueChunks: 0,
+				UniqueCSize:       0,
+				UniqueSize:        0,
+			},
+		},
+		Encryption: types.Encryption{
+			Mode: "repokey-blake2",
+		},
+		Repository: types.Repository{
+			ID:           "01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+			LastModified: "2024-12-02T10:28:45.000000",
+			Location:     "/home/test/arcotest/dest/encrypted_with_123",
+		},
+		SecurityDir: "/home/test/.config/borg/security/01267068cadb779b15931dc6ff82c1f4ccebc39acc1f1f51dc12d4bba3a0decf",
+	},
+	wantErr: false,
+}
+
+var withJsonError = testBorgInfo{
+	name:      "Call info - with json error",
+	cmdResult: []byte(`{{}`),
+	result:    nil,
+	wantErr:   true,
+}
+
 func TestBorgInfo(t *testing.T) {
 	var b Borg
 	var cr *mockborg.MockCommandRunner
@@ -195,7 +310,10 @@ func TestBorgInfo(t *testing.T) {
 	tests := []testBorgInfo{
 		emptyRepo,
 		nonEmptyRepo,
-		repoWithWarning,
+		withWarning,
+		withMultilineWarning,
+		withMultilineWarningAndSpaces,
+		withJsonError,
 	}
 
 	for _, tt := range tests {
