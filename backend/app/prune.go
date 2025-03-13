9cf74c2c960b425dc00938d781aad46e12bb4594
rapha <dev@uupi.cloud>
2025-03-13 13:56:17 +0100

    feat: alles neu 2

--- backend/app/prune.go
+++ backend/app/prune.go
@@ -440,7 +440,7 @@ func (b *BackupClient) examinePrune(bId types.BackupId, pruningRuleOpt safetypes
 					SetWillBePruned(true).
 					Save(b.ctx)
 				if err != nil {
-					return 0, rollback(tx, fmt.Errorf("failed to update newnew: %w", err))
+					return 0, rollback(tx, fmt.Errorf("failed to update newnew 2: %w", err))
 				}
 
 				cntToFalse, err := tx.Archive.
