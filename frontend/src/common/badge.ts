2a54897f3838ef5da6b146140ef4d1844391f1a9
rapha <dev@uupi.cloud>
2025-08-07 16:20:45 +0200

    feat(frontend): add error and warning badges

--- frontend/src/common/badge.ts
+++ frontend/src/common/badge.ts
@@ -1,5 +1,6 @@
 import { diffDays, diffHours, offset, removeOffset } from "@formkit/tempo";
 import { RepoType } from "./repository";
+import * as state from "../../bindings/github.com/loomi-labs/arco/backend/app/state";
 
 /**
  * Returns the style for a badge based on the difference between the current date and the given date.
@@ -53,4 +54,27 @@ export function toRepoTypeBadge(type: RepoType): string {
     default:
       return "badge border-arco-purple-500 text-arco-purple-500 bg-transparent truncate";
   }
+}
+
+/**
+ * Returns the style for an error state badge.
+ * @param repoState The repository state.
+ */
+export function toErrorStateBadge(repoState: state.RepoState): string {
+  if (repoState.errorType === state.RepoErrorType.RepoErrorTypeNone || repoState.errorType === state.RepoErrorType.$zero) {
+    return "";
+  }
+  return "badge badge-error dark:border-error dark:bg-transparent dark:text-error truncate cursor-pointer";
+}
+
+/**
+ * Returns the style for a warning state badge.
+ * @param repoState The repository state.
+ * @param isDismissed Whether the warning is dismissed.
+ */
+export function toWarningStateBadge(repoState: state.RepoState, isDismissed: boolean): string {
+  if (!repoState.hasWarning || isDismissed) {
+    return "";
+  }
+  return "badge badge-warning dark:border-warning dark:bg-transparent dark:text-warning truncate cursor-pointer";
 }
