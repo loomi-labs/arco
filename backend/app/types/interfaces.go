6d93e0dd75d0a0a01e012d7457311aab4bb2eff8
rapha <dev@uupi.cloud>
2025-08-22 19:45:35 +0200

    feat: add interfaces.go with go:generate directive

---
+++ backend/app/types/interfaces.go
@@ -0,0 +1,11 @@
+//go:build !integration
+
+package types
+
+import "context"
+
+//go:generate mockgen -destination=mocks/event_emitter.go -package=mocks . EventEmitter
+
+type EventEmitter interface {
+	EmitEvent(ctx context.Context, event string)
+}
